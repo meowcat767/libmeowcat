b93906ca893f65d2cb2df597bdb72728b5534636
Ben <meowcat@meowcat.site>
2026-04-26 10:44:56 +0100

    Example of PathsEx

--- src/main/java/site/meowcat/examples/files/PathsExExample.java
+++ src/main/java/site/meowcat/examples/files/PathsExExample.java
@@ -2,8 +2,16 @@ package site.meowcat.examples.files;
 
 import site.meowcat.system.files.PathsEx;
 
+/**
+ * Example using PathsEx to get various system paths. Note that the appName parameters are just examples; in production, use your real app name.
+ * @author meowcat767
+ */
 public class PathsExExample {
     public static void main(String[] args) {
         System.out.println("Your home directory is:" + PathsEx.home());
+        System.out.println("Your documents directory is:" + PathsEx.documents());
+        System.out.println("Your data directory is:" + PathsEx.dataDir("libmeowcat")); // Note that I'm using example appNames; in prod use your real one.
+        System.out.println("Your config directory is:" + PathsEx.configDir("MyCoolApp"));
+        System.out.println("Your cache directory is:" + PathsEx.cacheDir("DukesDungeon"));
     }
 }
